aac6fb8073910843ef85bd8a7ef04cb284309834
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-06 11:40:44 +0300

    API call to get a particular category of news

--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from flask import render_template
 from app import app
+from .request import get_news
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_news = get_news('popular')
+    print(popular_news)
+    title = 'news1234.com'
+    return render_template('index.html', title = title,popular = popular_news)
+
 
 # Views
 @app.route('/')

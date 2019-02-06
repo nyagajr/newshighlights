5e7e063b96ac0ceed739e6e85336763a6466bd4a
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-06 14:03:40 +0300

    a get function that takes in id and returns the object

--- app/request.py
+++ app/request.py
@@ -7,7 +7,7 @@ News = news.News
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
-# Getting the movie base url
+# Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
 
 def get_news(category):
@@ -54,3 +54,24 @@ def process_results(news_list):
             news_results.append(news_object)
 
     return news_results
+
+
+def get_news(id):
+    get_news_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_news_details_url) as url:
+        news_details_data = url.read()
+        news_details_response = json.loads(news_details_data)
+
+        news_object = None
+        if news_details_response:
+            id = news_details_response.get('id')
+            title = news_details_response.get('original_title')
+            overview = news_details_response.get('overview')
+            poster = news_details_response.get('poster_path')
+            vote_average = news_details_response.get('vote_average')
+            vote_count = news_details_response.get('vote_count')
+
+            news_object = News(id,title,overview,poster,vote_average,vote_count)
+
+    return news_object
